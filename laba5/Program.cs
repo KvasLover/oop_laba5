d70eb6ee728c25ff7a1acf06e68c72d628cd3540
KvasLover <vadimkurb22@gmail.com>
2020-11-15 15:16:58 +0300

    Добавьте файлы проекта.

---
+++ laba5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace laba5
+{
+    public abstract class Quest
+    {
+        public int Name { get; set; }
+    }    
+    public class Exam : Quest
+    {
+
+    }
+    public class Final_Exam : Exam
+    {
+
+    }
+    public class Test : Quest
+    {
+
+    }
+    public sealed class Question : Test
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
